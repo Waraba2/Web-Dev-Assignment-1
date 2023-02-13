d2eddd0460afa2e3e607a6e0accff50a31f86ae9
hamidouwaraballo <ballowara@gmail.com>
2023-02-13 17:56:01 -0500

    home page done

--- src/components/page3Link/Page3Link.js
+++ src/components/page3Link/Page3Link.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Link} from "react-router-dom"
-
+import './Page3Link.css'
 const Page3Link = () => {
     return (
         <div className="Page3Link">
--- src/containers/header/Header.js
+++ src/containers/header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Header.css'
 import {WhatsUp} from "../../components";
 
 const Header = () => {
